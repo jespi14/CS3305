b39697d8cce4d50d552676902349177037b3a3ef
jespi14 <javierespinoza79@gmail.com>
2023-06-21 21:04:06 -0400

    Finished most of the work

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,11 +1,11 @@
 /*
 This class define a linked list that stores integer values.
 */
-
-import org.w3c.dom.Node;
-
-import java.util.Scanner;
-
+//Name:Javier Espinoza
+// Class:CS 3305/Section W01
+// Term:Summer 2023
+// Instructor:Prof. Majeed
+// Assignment:1
 public class LinkedList
 {
 
@@ -35,62 +35,116 @@ public class LinkedList
    //method #1: add first node
    public void addFirstNode(int data)
    {
-
-
       if(tail==null)
          head=tail=new Node(data); // if the list is empty
       else {
-         Node newNode= new Node(data);
+         Node newNode= new Node(data);//makes a new node with the data
 
-         newNode.next=head;
-         head=newNode;
-
-
-
-
+         newNode.next=head;//the new node will point to the head of the linked list
+         head=newNode;// the head of the node will be the newly created node
       }
-      //complete this method
    }
       
    //method #2: add node at index
    public void addAtIndex(int index, int data)
    {
-         //complete this method
+      Node temp;//makes a pointer node
+      int size = countNodes();
+      if(index<=1){
+         //if the user selects index 0, then will call the "addFirstNode" method
+         addFirstNode(data);
+      }
+      else if(index>size){
+         //if the index is larger than the size then will call the "addLastNode"
+         addLastNode(data);
+      }
+      else{
+         for(int i=1;i<=index-1;i++){
+            temp=head;//makes the pointer, point to the head
+
+
+            temp=temp.next;//after each loop, will traverse the Linked List, stopping at index-1
+            Node newNode = new Node(data);//Once stopped at the location, it creates a new node with the data
+            newNode.next=temp.next;//once the node is created it, it will point to the next node
+         }
+
+
+      }
+
    }
       
    //method #3: remove first node
    public void removeFirstNode()
    {
-         //complete this method
+      removeAtIndex(0);//will call the removeAtIndex at position 0;
+
    }
       
    //method #4: remove last node
    public void removeLastNode()
    {
-         //complete this method
+      int size= countNodes();// will call countNodes to see how big the list is and will store this in an int
+      removeAtIndex(size-1);//Once the size of the list is found it will remove the node at the last position -1
+
    }
     
    //method #5: remove node at index
-   public void removeAtIndex(int index)
-   {
-         //complete this method
+   public void removeAtIndex(int index) {
+      Node temp;//pointer node
+      temp = head;//pointer node will point to the head
+      int size = countNodes();// will get the size of the linked list
+      if(index==0){
+         removeFirstNode();//if index is zero then it will remove the first node
+      }
+      else if (index>=size){
+         removeLastNode();//if the index is greater than the size, then it will remove the last node
+      }
+      else{
+         for(int i = 0;i<index;i++){
+
+            temp=temp.next;// for each iteration, it will move down the linked list until it get to the index desired
+         }
+         temp=null;//will change the data to nothing
+         temp.next=temp.next.next;//will move the next node to the next node after
+
+      }
+
+
    }
           
    //method #6: countNodes
-   public int countNodes()
-   {
-         int listSize= 0;
-         //complete this method
-         //this methods returns the list size
-         return listSize;
-         
+   public int countNodes() {
+      int listSize = 0;//a field to count the value of how big the list is
+      if(head==null){
+         return 0;//if the list is empty them it will send a value of 0
+      }
+      else{
+            Node temp;//temp node to point
+            temp=head;//will count the head
+            while(temp!=null)//This loop will run while the pointer node is not empty
+            {
+               listSize++;//will count the node
+               temp = temp.next;// will move the pointer to the next node
+            }
+      }
+
+      //complete this method
+    return listSize;
    }
-   
-   //method #7: pritnInReverse  (Recursive method)
+
+
+
+   //method #7: printInReverse  (Recursive method)
    public void printInReverse(Node L)
    {
-         //complete this method as recursive methods
-   }   
+      if(L==null){//if the node is null then retunr
+         return;
+      }
+      printInReverse(L.next);//if there is data in the node , then call this method until there is a node with no data
+      System.out.print(L.data+" ");//once we find a node with no data, we will print that last node with data and print all the way to the head
+
+
+   }
 
    //================= end of your part ==============
 
--- src/myTest.java
+++ src/myTest.java
@@ -1,4 +1,9 @@
-// Add your header here
+//Name:Javier Espinoza
+// Class:CS 3305/Section W01
+// Term:Summer 2023
+// Instructor:Prof. Majeed
+// Assignment:1
+
 
 import java.util.Scanner;
 
@@ -7,28 +12,105 @@ public class myTest
    public static void main (String[] args)
    {
       LinkedList myList = new LinkedList(); //create a list object
-   
-      for (int i=1; i <= 5; i++) //add 5 nodes to the list
-      {
-         myList.addFirstNode(i*10);
-      } 
-      
-      //print out the list content
-      System.out.print("My test list values are:  ");
-      myList.printList();
-      System.out.println();    
-   }
-   /*
-   public static void Menu(){
       int choice;
       do {
          Scanner sc = new Scanner(System.in);
-         System.out.println("--------MAIN MENU--------\n1 - Add First Node\n2 - Add Last Node\n3 - Add At Index\n4 - Remove First Node\n5 - Remove Last Node\n6 - Remove At Index\n7 - Print List Size\n8 - Print List(Forward)\n9 - Print List In Reverse\n10 - Exit Program");
+         System.out.println("\n--------MAIN MENU--------\n1 - Add First Node\n2 - Add Last Node\n3 - Add At Index\n4 - Remove First Node\n5 - Remove Last Node\n6 - Remove At Index\n7 - Print List Size\n8 - Print List(Forward)\n9 - Print List In Reverse\n10 - Exit Program");
          choice=sc.nextInt();
+         if(choice==1){
+            System.out.println("Please Enter a value");
+            int num = sc.nextInt();
+            System.out.println("Testing method addFirstNode");
+            System.out.println("Contents before adding: ");
+            myList.printList();
+            System.out.println();
+            System.out.println("Contents after adding: ");
+            myList.addFirstNode(num);
+            myList.printList();
+
+
+
+         }
+         else if (choice==2){
+            System.out.println("Please Enter a value");
+            int num = sc.nextInt();
+            System.out.println("Testing method addLastNode");
+            System.out.println("Contents before adding: ");
+            myList.printList();
+            System.out.println();
+            System.out.println("Contents after adding: ");
+            myList.addLastNode(num);
+            myList.printList();
+         }
+         else if (choice==3){
+            System.out.println("Please enter a position");
+            int pos=sc.nextInt();
+            System.out.println("Please Enter a value");
+            int num = sc.nextInt();
+            System.out.println("Testing method addAtIndex");
+            System.out.println("Contents before adding: ");
+            myList.printList();
+            System.out.println();
+            System.out.println("Contents after adding: ");
+            myList.addAtIndex(pos,num);
+            myList.printList();
+         }
+         else if (choice==4){
+            System.out.println("Removing First Node");
+            System.out.println("Testing method RemoveFirstNode");
+            System.out.println("Contents before Remove: ");
+            myList.printList();
+            System.out.println();
+            System.out.println("Contents after Remove: ");
+            myList.removeFirstNode();
+            myList.printList();
+         }
+         else if (choice==5){
+            System.out.println("Removing Last Node");
+            System.out.println("Testing method RemoveLastNode");
+            System.out.println("Contents before Remove: ");
+            myList.printList();
+            System.out.println();
+            System.out.println("Contents after Remove: ");
+            myList.removeLastNode();
+            myList.printList();
+         }
+         else if (choice==6){
+
+            System.out.println("Please enter a position");
+            int pos=sc.nextInt();
+            System.out.println("Removing at index"+pos);
+            System.out.println("Contents before Remove: ");
+            myList.printList();
+            System.out.println();
+            System.out.println("Contents after Remove: ");
+            myList.removeAtIndex(pos);
+            myList.printList();
+         }
+         else if (choice==7){
+            System.out.println("List size\n"+myList.countNodes());
+         }
+         else if (choice==8){
+            System.out.println("Printing List");
+            myList.printList();
+         }
+         else if(choice==9){
+            System.out.println("Print list in reverse");
+            myList.printInReverse(myList.head);
+         }
+         else if(choice==10){
+            System.out.println("Thank you for using my program");
+         }
+         else{
+            throw new IndexOutOfBoundsException("Please enter the correct value ");
+         }
       }while(choice!=10);
 
-
    }
-   */
+
+
+
+
+
 }
     
